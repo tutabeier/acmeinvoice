05ea8daccd8a021f5ea2902ccf317a5d74401c5b
Lucas Beier <lucas.beier@gmail.com>
2017-04-08 17:12:07 -0300

    Removing unused imports

--- src/main/java/acmeinvoice/repository/CustomerRepository.java
+++ src/main/java/acmeinvoice/repository/CustomerRepository.java
@@ -3,11 +3,6 @@ package acmeinvoice.repository;
 import acmeinvoice.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import java.util.List;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {}
