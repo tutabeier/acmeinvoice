6cdcf86cbde1c28774cbf23ac51dc0caaa4db36f
Lucas Beier <lucas.beier@gmail.com>
2017-04-07 23:24:35 -0300

    gitlab ci fixes

--- src/main/java/acmeinvoice/repository/CustomerRepository.java
+++ src/main/java/acmeinvoice/repository/CustomerRepository.java
@@ -10,24 +10,4 @@ import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Repository
-@Transactional(readOnly = true)
-public class CustomerRepository{
-    @PersistenceContext
-    private EntityManager entityManager;
-
-    @Transactional
-    public Customer save(Customer customer) {
-
-        if (customer.getId() == null) {
-            entityManager.persist(customer);
-            return customer;
-        } else {
-            return entityManager.merge(customer);
-        }
-    }
-
-    @Transactional
-    public List<Customer> findAll() {
-        return entityManager.createQuery("select c from Customer c", Customer.class).getResultList();
-    }
-}
+public interface CustomerRepository extends JpaRepository<Customer, Long> {}
