c2e2143233108347018b93886c940d1efc5e5811
Lucas Beier <lucas.beier@gmail.com>
2017-04-10 03:38:21 -0300

    Adds stereotype to Address Repository

--- src/main/java/acmeinvoice/repository/AddressRepository.java
+++ src/main/java/acmeinvoice/repository/AddressRepository.java
@@ -2,5 +2,7 @@ package acmeinvoice.repository;
 
 import acmeinvoice.model.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AddressRepository extends JpaRepository<Address, Long> {}
